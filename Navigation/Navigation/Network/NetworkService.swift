909736e5467d9ff7b219a60389aae6b2538dca48
SokolovAN <sokolovan@infotecs.ru>
2022-04-23 12:39:20 +0300

    Реализация задания JSON. Codable. Модели данных

--- Navigation/Navigation/Network/NetworkService.swift
+++ Navigation/Navigation/Network/NetworkService.swift
@@ -7,20 +7,21 @@
 
 import Foundation
 
-
-enum AppConfiguration: String {
-    case people = "https://swapi.dev/api/people/8"
-    case starships = "https://swapi.dev/api/starships/3"
-    case planets = "https://swapi.dev/api/planets/5"
-}
-
 /// @brief Сервис для выполнения сетевых задач
-struct NetworkService {
+class NetworkService {
+    private enum AppConfiguration: String {
+        case people = "https://swapi.dev/api/people/8"
+        case starships = "https://swapi.dev/api/starships/3"
+        case planets = "https://swapi.dev/api/planets/5"
+    }
+
     static var shared: NetworkService = {
         let instance = NetworkService()
         return instance
     }()
+    
     private init() {}
+    
     
     /// @brief  Определение url для подключения
     func detectUrlForConnection() -> String {
@@ -40,36 +41,52 @@ struct NetworkService {
         return appConfiguration.rawValue
     }
     
+    /// @brief Запрос на получение данные, ответ отадем сразу же в декодер, здесь не обрабатываем
+    func sendRequest(url: String, responseHandler: @escaping CompletionHttpResponseHandler ) {
+        self.sendRequestImpl(url: url, responseHandler: responseHandler)
+    }
+    
+    /// @todo чтобы не сломать предыдующую домашку сохранил сигнатуру вызова
+    ///  Реализацию вынес в отдельный класс, здесь идет только обработка ответа
+    func sendRequest(url: String) {
+        self.sendRequestImpl(url: url, responseHandler: { result in
+            switch result {
+            case .success(let data):
+                print(HttpDecoder.shared.decodeData(mode: .simple, data: data))
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        })
+    }
+    
+    
     /// @brief отправка запроса на указанный url
     /// @todo в задачи нет условия работы с конфигурациями или кастомными запросами
     /// поэтому создал дефолтную сессию через синглтон
-    func sendRequest(url: String) {
+    /// Декодирование данных перенесем на уровень модели
+    private func sendRequestImpl(url: String, responseHandler: @escaping CompletionHttpResponseHandler ) {
         guard let url = URL(string: url) else {
-            print("URL is nil")
+            responseHandler(.failure(.badFormatRequest))
             return
         }
+        
         // Создаем сессию и ставим задачу на выполнения
         // в callback обрабатываем ответ
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
-            /// @todo поскольку никаких действий на главном потоке делать не нужно то обрабатываем ответ  в бэкграунде
             if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
-                print("Http code: \(httpResponse.statusCode)")
+                responseHandler(.failure(.badAnswer))
                 return
             }
             
             guard let data = data else {
-                print("Data is nil")
+                responseHandler(.failure(.noData))
                 return
             }
-            
-            guard let serverData = String(data: data, encoding: .utf8) else {
-                print("Error parse data from server")
-                return
-            }
-            
-            print("Server data: \(serverData)")
+            responseHandler(.success(data))
         }
+
         session.resume()
+        
     }
 }
 
